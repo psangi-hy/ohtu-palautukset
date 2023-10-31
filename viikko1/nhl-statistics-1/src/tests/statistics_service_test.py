665b68cfeed57828e0bb605da90d64fde0b5c229
Pyry Sangi <pyry.sangi@helsinki.fi>
2023-11-01 01:21:13 +0200

    Add tests for StatisticsService.top sorting criteria

--- viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -1,5 +1,5 @@
 import unittest
-from statistics_service import StatisticsService, sort_by_points
+from statistics_service import StatisticsService, sort_by_points, SortBy
 from player import Player
 
 class PlayerReaderStub:
@@ -21,9 +21,12 @@ class TestStatisticsService(unittest.TestCase):
         points = sort_by_points(player)
         self.assertEqual(points, 4 + 12)
 
+    def assert_player_equal(self, a, b):
+        self.assertEqual(str(a), str(b))
+
     def test_search_existing(self):
         res = self.stats.search("etz")
-        self.assertEqual(str(res), str(Player("Gretzky", "EDM", 35, 89)))
+        self.assert_player_equal(res, Player("Gretzky", "EDM", 35, 89))
 
     def test_search_nonexisting(self):
         res = self.stats.search("Horvat")
@@ -38,8 +41,13 @@ class TestStatisticsService(unittest.TestCase):
         invented_team = self.stats.team("INV")
         self.assertEqual(len(invented_team), 0)
 
-    def test_top(self):
-        players_sorted = [
+    def assert_player_list_equal(self, a, b):
+        self.assertEqual(len(a), len(b))
+        for i in range(len(a)):
+            self.assert_player_equal(a[i], b[i])
+
+    def test_top_by_points(self):
+        by_points = [
             Player("Gretzky", "EDM", 35, 89),
             Player("Lemieux", "PIT", 45, 54),
             Player("Yzerman", "DET", 42, 56),
@@ -48,6 +56,34 @@ class TestStatisticsService(unittest.TestCase):
         ]
         for i in range(0, 6):
             res = self.stats.top(i)
-            self.assertEqual(len(res), i)
-            for j in range(i):
-                self.assertEqual(str(res[j]), str(players_sorted[j]))
+            res_points = self.stats.top(i, SortBy.POINTS)
+            self.assertEqual(res, res_points)
+            self.assert_player_list_equal(res, by_points[:i])
+
+    def test_top_by_goals(self):
+        by_goals = [
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Gretzky", "EDM", 35, 89),
+            Player("Semenko", "EDM", 4, 12)
+        ]
+        for i in range(0, 6):
+            res = self.stats.top(i, SortBy.GOALS)
+            self.assert_player_list_equal(res, by_goals[:i])
+
+    def test_top_by_assists(self):
+        by_assists = [
+            Player("Gretzky", "EDM", 35, 89),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Semenko", "EDM", 4, 12)
+        ]
+        for i in range(0, 6):
+            res = self.stats.top(i, SortBy.ASSISTS)
+            self.assert_player_list_equal(res, by_assists[:i])
+
+    def test_top_invalid_sort_by(self):
+        for i in range(0, 6):
+            self.assertIsNone(self.stats.top(i, 4))
