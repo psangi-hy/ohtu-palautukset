d6cd7f78e538510d7778772ebe60ca145429d8aa
Pyry Sangi <pyry.sangi@helsinki.fi>
2023-11-13 17:32:20 +0200

    Tehtävä 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,22 +1,13 @@
 import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
 
-    players_filtered = filter(
-            lambda p: p.nationality == "FIN",
-            (Player(player_dict) for player_dict in response))
-
-    players_sorted = sorted(
-            players_filtered,
-            key=lambda p: p.goals + p.assists,
-            reverse=True)
-
-    for player in players_sorted:
-        if player.nationality == "FIN":
-            print(player)
+    for player in stats.top_scorers_by_nationality("FIN"):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         for field in ["name", "nationality", "goals", "assists", "team"]:
@@ -7,3 +9,29 @@ class Player:
         return "{:25} {:5} {:3}  + {:3}  ={:4}".format(
                 self.name, self.team, self.goals, self.assists,
                 self.goals + self.assists)
+
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        return [Player(player_dict) for player_dict in response]
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players_filtered = filter(
+                lambda p: p.nationality == "FIN",
+                self.reader.get_players())
+
+        players_sorted = sorted(
+                players_filtered,
+                key=lambda p: p.goals + p.assists,
+                reverse=True)
+
+        return players_sorted
